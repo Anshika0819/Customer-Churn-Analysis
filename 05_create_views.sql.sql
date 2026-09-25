63ae2aae357cf9c296be1fb2037464f001ab8668
Anshika0819 <namishka2106@gmail.com>
2026-09-25 14:49:50 +0530

    Delete 05_create_views.sql.sql

--- 05_create_views.sql.sql
+++
@@ -1,6 +0,0 @@
-create view vw_ChurnData as
-select * from prod_Churn where Customer_Status IN ('Churned', 'Stayed')
-go
-create view vw_JoinData as
-select * from prod_Churn where	Customer_Status = 'Joined'
-go
